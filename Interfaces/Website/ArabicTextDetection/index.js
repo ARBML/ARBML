1bd074ac52ff127048612d3e857d98269a5fdab8
wael <wael.tellat@gmail.com>
2023-01-08 19:46:31 +0100

    PR review

--- Interfaces/Website/ArabicTextDetection/index.js
+++ Interfaces/Website/ArabicTextDetection/index.js
@@ -6,6 +6,23 @@ $(document).ready(function () {
   var imageSrc = undefined;
 
   /*
+  * Choose an image randomly. Exclude the current image.
+  */
+  function getRandomImage() {
+    const availableImages = [
+      'images/united_nations_intro.png',
+      'images/united_nations_intro_amiri.png',
+      'images/wikipedia_arabic_language.png',
+      'images/ibn_batouta_cover.png',
+      'images/ar_news.png',
+      'images/ar_physics.png',
+    ];
+    var imagesExceptCurrent = availableImages.filter(item => !item.endsWith(imageSrc));
+    var index = Math.floor(Math.random() * imagesExceptCurrent.length);
+    return imagesExceptCurrent[index];
+  }
+
+  /*
   * Draw the uploaded image
   */
   function handleFiles(e) {
@@ -20,10 +37,11 @@ $(document).ready(function () {
     }
     img.src = url;
     imageSrc = url;
+    detect();
   }
 
 
-  function loadInitialImg() {
+  function loadRandomImage() {
     var img = new Image();
 
     img.onload = function () {
@@ -32,18 +50,21 @@ $(document).ready(function () {
       ctx.drawImage(img, 0, 0, 512, img.height / scale);
     };
 
-    img.src = 'images/united_nations_intro.png';
-    imageSrc = img.src;
+    imageSrc = getRandomImage();
+    img.src = imageSrc;
+    console.log(`Selected image: ${imageSrc}`)
   }
 
   /*
   * Start the models and warm up
   */
   async function start() {
+    $("#loader-result").hide();
+
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    loadInitialImg();
+    loadRandomImage();
 
     // Load the model
     console.log('Loading Tesseract model');
@@ -58,13 +79,20 @@ $(document).ready(function () {
     $("#outputs").show();
     $("#output").val("");
 
+    detect()
+
   }
 
   /*
   * given an image with arabic text, detect text.
   */
   async function detect() {
+    $("#loader-result").show();
+    $(".output-text").hide()
+    console.log(`Start scanning image: ${imageSrc}`)
     const { data: { text } } = await worker.recognize(imageSrc);
+    $("#loader-result").hide();
+    $(".output-text").show();
     console.log(text);
     $(".output-text").text(text);
   }
@@ -75,10 +103,13 @@ $(document).ready(function () {
     $(".preview").focus().trigger('click');
   });
 
+  $(".change").on("click", function () {
+    loadRandomImage();
+    $(".output-text").text('');
+    detect()
+  });
+
   var input = document.getElementById('preview');
   input.addEventListener('change', handleFiles, false);
 
-  $(".submit").on("click", function () {
-    detect()
-  });
 });
